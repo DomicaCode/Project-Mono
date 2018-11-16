538f2cf1488a0dce6c8917094e0978ab8b4fbd2a
DomicaCode <36825658+DomicaCode@users.noreply.github.com>
2018-11-16 20:10:24 +0100

    Initial upload

---
+++ Data/Entities/VehicleMakeEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class VehicleMakeEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Abrv { get; set; }
+    }
+}
